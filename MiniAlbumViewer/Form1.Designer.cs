850338e655e895f130ce53ae3f3d641ae040ef40
sunbang123 <93497158+sunbang123@users.noreply.github.com>
2026-05-28 21:43:33 +0900

    style: UI Settings

--- MiniAlbumViewer/Form1.Designer.cs
+++ MiniAlbumViewer/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            splitContainer1 = new SplitContainer();
+            panel1 = new Panel();
+            BtnFlip = new Button();
+            BtnRotate = new Button();
+            BtnFolder = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(flowLayoutPanel1);
+            splitContainer1.Panel1.Controls.Add(panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(pictureBox1);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 266;
+            splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(BtnFolder);
+            panel1.Controls.Add(BtnRotate);
+            panel1.Controls.Add(BtnFlip);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 342);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(266, 108);
+            panel1.TabIndex = 0;
+            // 
+            // BtnFlip
+            // 
+            BtnFlip.Dock = DockStyle.Bottom;
+            BtnFlip.Location = new Point(0, 74);
+            BtnFlip.Name = "BtnFlip";
+            BtnFlip.Size = new Size(266, 34);
+            BtnFlip.TabIndex = 0;
+            BtnFlip.Text = "좌우 반전";
+            BtnFlip.UseVisualStyleBackColor = true;
+            // 
+            // BtnRotate
+            // 
+            BtnRotate.Dock = DockStyle.Bottom;
+            BtnRotate.Location = new Point(0, 40);
+            BtnRotate.Name = "BtnRotate";
+            BtnRotate.Size = new Size(266, 34);
+            BtnRotate.TabIndex = 1;
+            BtnRotate.Text = "90도 회전";
+            BtnRotate.UseVisualStyleBackColor = true;
+            // 
+            // BtnFolder
+            // 
+            BtnFolder.Dock = DockStyle.Bottom;
+            BtnFolder.Location = new Point(0, 6);
+            BtnFolder.Name = "BtnFolder";
+            BtnFolder.Size = new Size(266, 34);
+            BtnFolder.TabIndex = 2;
+            BtnFolder.Text = "폴더 열기";
+            BtnFolder.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(266, 342);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(530, 450);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FolderBrowserDialog folderBrowserDialog1;
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private Button BtnFolder;
+        private Button BtnRotate;
+        private Button BtnFlip;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
     }
 }
